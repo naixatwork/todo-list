026da359f43d58df9ef3715abd7734953ed45f0c
naixatwork <naixatwork@gmail.com>
2021-07-24 19:43:35 +0430

    feat(auth): introduced auth component

---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthComponent } from './auth.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: AuthComponent,
+  },
+];
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthModule {}

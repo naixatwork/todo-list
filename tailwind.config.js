e6c8c87fff719ad66e2766eabdb03aafd4e42e63
naixatwork <naixatwork@gmail.com>
2021-07-24 19:42:16 +0430

    feat(style): installed tailwind with ngneat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+process.env.TAILWIND_MODE = guessProductionMode() ? "build" : "watch";
+
 module.exports = {
+  prefix: "",
   important: true,
   mode: "jit",
-  purge: [["./src/**/*.{html,ts,scss}"]],
+  purge: {
+    content: ["./src/**/*.{html,ts,css,scss,sass,less,styl}"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
+        roboto: ["'Roboto'", "sans-serif"],
         zen: ["'Zen Loop'"],
         pacifico: ["Pacifico"],
       },

b4fd16cbeee70a5abd7076d80dcbc1d373974e7d
naixatwork <naixatwork@gmail.com>
2021-07-27 14:00:05 +0430

    chore(analytic): added mock delay to compeleted request

--- src/app/modules/analytic/analytic.service.ts
+++ src/app/modules/analytic/analytic.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RawTask } from '#modules/task/task/task.model';
 import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable()
 export class AnalyticService {
   constructor(private readonly http: HttpClient) {}
 
   public getCompletedTasks(): Observable<Array<RawTask>> {
-    return this.http.get<Array<RawTask>>('compeleted');
+    return this.http.get<Array<RawTask>>('compeleted').pipe(delay(500));
   }
 }
